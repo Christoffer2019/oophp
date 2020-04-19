38b27a01d8b63bf4f0cf71bc027f7079261d3ade
Christoffer Lymalm <chly19@student.bth.se>
2020-04-19 13:28:04 +0100

    Added functions for changing image size based on the current width of the browser window and hiding the link to top button when the top of the page is displayed in htdocs/js/main.js.

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,8 +1,53 @@
 /**
- * To show off JS works and can be integrated.
+ * Main js
  */
-(function() {
+(function () {
     "use strict";
 
-    console.info("main.js ready and loaded.");
-})();
+    var mePresentation = document.getElementById("me_presentation");
+    var about = document.getElementById("about_img");
+
+    changeImageSize();
+    window.onresize = changeImageSize;
+    window.onscroll = showLinkToTop;
+
+    function changeImageSize() {
+        /**
+         * Change image size based on the current width of the browser window.
+         */
+        var width = window.innerWidth;
+        var pathStart = "img/minimalistic/";
+        var pathEnd = ".jpg";
+
+        if (width >= 700) {
+            if (mePresentation) {
+                mePresentation.src = pathStart + "me-presentation480x539" + pathEnd;
+            }
+            if (about) {
+                about.src = pathStart + "about600x399" + pathEnd;
+            }
+        } else {
+            if (mePresentation) {
+                mePresentation.src = pathStart + "me-presentation1080x1212" + pathEnd;
+            }
+            if (about) {
+                about.src = pathStart + "about1080x719" + pathEnd;
+            }
+        }
+    }
+
+    function showLinkToTop() {
+        /**
+         * Hide the link to top button when the top of the page is
+         * displayed.
+         */
+        var currentScrollPos = window.pageYOffset;
+        var linkToTop = document.getElementById("link-to-top-page");
+
+        if (currentScrollPos > 0) {
+            linkToTop.style.display = "inline-block";
+        } else {
+            linkToTop.style.display = "none";
+        }
+    }
+}());
